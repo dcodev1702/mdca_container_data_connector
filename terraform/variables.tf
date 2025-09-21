cab74dba9dabea9fd927705ef8167a1dba4a59be
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2025-09-21 19:50:10 -0400

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,37 +3,22 @@
 variable "location" {
   description = "Azure region for resource deployment"
   type        = string
-  default     = "East US 2"
 }
 
 variable "vm_size" {
   description = "Size of the Azure virtual machine"
   type        = string
-  default     = "Standard_D2s_v3"
 }
 
 variable "admin_username" {
   description = "Administrator username for the VM"
   type        = string
-  default     = "lorenzoadm"
 }
 
 variable "os_disk_size_gb" {
   description = "Size of the OS disk in GB"
   type        = number
   default     = 128
-}
-
-variable "environment" {
-  description = "Environment tag for resources"
-  type        = string
-  default     = "Demo"
-}
-
-variable "project" {
-  description = "Project tag for resources"
-  type        = string
-  default     = "MDCA"
 }
 
 variable "ssh_key_path" {
@@ -54,18 +39,6 @@ variable "tags" {
   default     = {}
 }
 
-# Optional variables for custom network configuration
-variable "vnet_address_space" {
-  description = "Address space for the virtual network"
-  type        = list(string)
-  default     = ["10.0.0.0/16"]
-}
-
-variable "subnet_address_prefix" {
-  description = "Address prefix for the subnet"
-  type        = list(string)
-  default     = ["10.0.1.0/24"]
-}
 
 # Syslog configuration
 variable "syslog_port" {
@@ -78,4 +51,54 @@ variable "allow_syslog_from_any" {
   description = "Allow syslog traffic from any source IP (set to false for production)"
   type        = bool
   default     = true
+}
+
+
+# Auth Token variables
+variable "mdca_auth_token" {
+  description = "Authentication token for MDCA log collector"
+  type        = string
+  sensitive   = true
+}
+
+variable "mdca_console_url" {
+  description = "MDCA console URL"
+  type        = string
+}
+
+variable "mdca_collector_name" {
+  description = "MDCA collector name"
+  type        = string
+}
+
+# Azure Core variables
+variable "vm_username" {
+  description = "VM admin username"
+  type        = string
+}
+
+variable "tag_env" {
+  description = "Environment tag"
+  type        = string
+}
+
+variable "tag_proj" {
+  description = "Project tag"
+  type        = string
+}
+
+# Network variables
+variable "network_vnet_cidr" {
+  description = "Virtual Network CIDR"
+  type        = list(string)
+}
+
+variable "vm_subnet_cidr" {
+  description = "VM Subnet CIDR"
+  type        = list(string)
+}
+
+variable "vm_private_ip" {
+  description = "VM private IP address"
+  type        = string
 }
